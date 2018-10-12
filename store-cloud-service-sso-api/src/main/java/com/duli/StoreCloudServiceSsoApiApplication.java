ae2eb0af3b19874435d761d317afda7a444df360
592306832@qq.com <592306832@qq.com>
2018-10-12 23:16:25 +0800

    sso

---
+++ store-cloud-service-sso-api/src/main/java/com/duli/StoreCloudServiceSsoApiApplication.java
@@ -0,0 +1,12 @@
+package com.duli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCloudServiceSsoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCloudServiceSsoApiApplication.class, args);
+    }
+}
